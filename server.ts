9191cc46ddba41ff4a0ea0c33af14fcba1a89ad4
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-11-24 23:52:20 +0900

    完成

--- server.ts
+++ server.ts
@@ -1,9 +1,38 @@
 import { Hono } from 'hono'
+import JSObstruction from 'npm:javascript-obfuscator'
+import minify from 'npm:babel-minify'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-    return c.text('Hello World!')
+app.get('/',async (c) => {
+    return c.html(await Deno.readTextFile('./index.html'));
+})
+
+app.post('/minify', async (c) => {
+    const code = c.req.body ? await c.req.text() : "console.log('You have no code!')";
+    let result = {
+        code: code
+    };
+
+    if (code) {
+        result = minify(code, {})
+    }
+
+    return c.json({
+        code: result.code
+    })
+})
+
+app.post('/obstruction', async (c) => {
+    let code = c.req.body ? await c.req.text() : "console.log('You have no code!')";
+
+    if (code) {
+        code = JSObstruction.obfuscate(code).getObfuscatedCode()
+    }
+
+    return c.json({
+        code
+    })
 })
 
 Deno.serve(app.fetch)
